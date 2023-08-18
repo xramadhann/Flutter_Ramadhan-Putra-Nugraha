c3e334234fabb0e26511dd3697c1a95e08b64162
xramadhann <xxdhanz@gmail.com>
2023-08-18 16:14:23 +0700

    Update tantangan1.dart

--- 03_Fundamental Dart/Praktikum/tantangan1.dart
+++ 03_Fundamental Dart/Praktikum/tantangan1.dart
@@ -2,7 +2,7 @@ double sisi = 6.5;
 double panjang = 10.5;
 double lebar = 9.8;
 double kelilingpersegipanjang = 2 * panjang + lebar;
-double kelilingsegitiga = sisi * sisi * sisi;
+double kelilingsegitiga = sisi + sisi + sisi;
 void main() {
   print("Keliling persegi panjang : $kelilingpersegipanjang");
   print("Keliling segitiga : $kelilingsegitiga");
