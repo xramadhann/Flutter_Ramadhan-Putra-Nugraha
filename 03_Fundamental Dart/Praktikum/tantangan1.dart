f57785f068bff1453326f74bc059eef0ef1ce8d0
xramadhann <xxdhanz@gmail.com>
2023-08-18 16:11:49 +0700

    Tugas Jumat 8/18/2023

---
+++ 03_Fundamental Dart/Praktikum/tantangan1.dart
@@ -0,0 +1,9 @@
+double sisi = 6.5;
+double panjang = 10.5;
+double lebar = 9.8;
+double kelilingpersegipanjang = 2 * panjang + lebar;
+double kelilingsegitiga = sisi * sisi * sisi;
+void main() {
+  print("Keliling persegi panjang : $kelilingpersegipanjang");
+  print("Keliling segitiga : $kelilingsegitiga");
+}
