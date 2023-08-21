cb4af8ffcafd5043642f8787e220e90042ff068a
xramadhann <xxdhanz@gmail.com>
2023-08-21 15:57:20 +0700

    Update tantangan1.dart

--- 04_Branching - Looping - Function/Praktikum/tantangan1.dart
+++ 04_Branching - Looping - Function/Praktikum/tantangan1.dart
@@ -1,3 +1,6 @@
+// 1. buat sebuah function yang menghitung jumlah dari pembelian buku,  pensi, dan tas
+// 2. buatlah function yang menghitung diskon dari variabel total pembelian.
+// 3. buatlah function yang menghitung Total pembelian setelah diskon
 double buku = 10000;
 double pensil = 5000;
 double tas = 100000;
